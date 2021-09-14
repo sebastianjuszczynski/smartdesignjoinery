351fe0e593d6d64690b8bd28bda1c01aee96b2fa
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-14 13:52:32 +0100

    Added HashRouter, Switch, Route, Redirect to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import HeroImage from "./components/HeroImage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Information from "./components/Information";
+import Pricing from "./components/Pricing";
+import Gallery from "./components/Gallery";
+import Home from "./components/Home";
+import { toHome, toGallery, toPricing, toInformation } from "./routes";
+
 
 
 
 function App() {
   return (
-      <HeroImage />
+    <HashRouter>
+      <NavBar />
+    <Switch>
+      <Route path={toHome()}>
+        <Home />
+      </Route>
+      <Route path={toGallery()}>
+        <Gallery />
+      </Route>
+      <Route path={toInformation()}>
+        <Information />
+      </Route>
+      <Route path={toPricing()}>
+        <Pricing />
+      </Route>
+      <Route>
+        <Redirect to={toHome()} />
+      </Route>
+    </Switch>
+  </HashRouter>
+      
   );
 }
 
