57bc01b57b17a199a840cef5b0f4f94677601692
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-14 13:49:20 +0100

    Component Home created

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroImage from "../HeroImage"
+
+const Home = () => {
+    return (
+        <>
+          <HeroImage />  
+        </>
+    )
+}
+
+export default Home;
