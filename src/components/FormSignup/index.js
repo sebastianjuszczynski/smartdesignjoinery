664f05d82a826b2a18611048c018f8572718115c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-18 16:56:27 +0100

    FormSignup created and styled

---
+++ src/components/FormSignup/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import validate from './validateInfo';
+import useForm from './useForm';
+import { Form, FormInputs, FormLabel, FormInput, FormTextarea, FormInputButton } from "./styled";
+
+
+const FormSignup = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
+
+  return (
+    <>
+      <Form className='form' noValidate>
+        <FormInputs>
+          <FormLabel>Username</FormLabel>
+          <FormInput
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+            value={values.username}
+            onChange={handleChange}
+          />
+          {errors.username && <p>{errors.username}</p>}
+        </FormInputs>
+        <FormInputs>
+          <FormLabel>Email</FormLabel>
+          <FormInput
+            type='email'
+            name='email'
+            placeholder='Enter your email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </FormInputs>
+        <FormInputs>
+          <FormLabel>Password</FormLabel>
+          <FormTextarea
+            name='message'
+            rows='7'
+            placeholder='Please type your request...'
+            value={values.password}
+            onChange={handleChange}>
+          </FormTextarea>
+          {errors.message && <p>{errors.message}</p>}
+        </FormInputs>
+        <FormInputButton type='submit' onClick={handleSubmit}>
+          Send
+        </FormInputButton>
+      </Form>
+    </>
+  );
+};
+
+export default FormSignup;
---
+++ src/components/FormSignup/styled.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const Form = styled.div`
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+export const FormInputs = styled.div`
+    margin-bottom: 8px;
+    width: 80%;
+    p {
+        font-size: 0.8rem;
+    margin-top: 0.5rem;
+    color: #f00e0e; 
+    }
+`;
+export const FormLabel = styled.div`
+    display: inline-block;
+    font-size: 12.8px;
+    margin-bottom: 6px;
+    color: #000;
+`;
+export const FormInput = styled.input`
+    display: block;
+    padding-left: 10px;
+    outline: none;
+    border-radius: 2px;
+    height: 40px;
+    width: 100%;
+    border: 1px solid black;
+    ::placeholder {
+        color: #595959;
+        font-size: 12px; 
+    }
+`;
+export const FormTextarea = styled.textarea`
+    display: block;
+    padding-left: 10px;
+    outline: none;
+    border-radius: 2px;
+    height: 80px;
+    width: 100%;
+    border: none;
+    font-size: 0.8rem;
+    border: 1px solid black; 
+    ::placeholder {
+        color: #595959;
+        font-size: 12px; 
+    }
+`;
+export const FormInputButton = styled.button`
+    width: 80%;
+    height: 50px;
+    margin-top: 10px;
+    border-radius: 2px;
+    background: linear-gradient(
+      90deg,
+      rgb(114, 66, 43) 0%,
+      rgb(70, 26, 4) 100%
+    );
+    outline: none;
+    border: none;
+    color: #fff;
+    font-size: 16px;
+    :hover {
+        cursor: pointer;
+        background: linear-gradient(
+            90deg,
+            rgb(154, 66, 43) 0%,
+            rgb(110, 26, 4) 100%
+    );    
+    }
+`;
