d2d0e5c48e00b9c485b9af8274e6c012c62ee2b5
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-20 17:14:24 +0100

    dependency added to FooterParagraph

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const FooterParagraph = styled.p`
-    position: ${({location}) => location === "/home" ? "relative" : "fixed"};
+    position: ${({location}) => location === "/home" || "/gallery" ? "relative" : "fixed"};
     bottom: 0;
     left: 0;
     right: 0;
