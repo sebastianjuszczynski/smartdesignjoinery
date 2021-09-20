d1b0c19daeddafbd7a807313c272d0c3cb916ce6
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-20 12:32:51 +0100

    Contact component created

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../Form/index';
+import FormHeader from '../FormHeader';
+
+const Contact = () => {
+    return (
+        <>
+            <FormHeader />
+            <Form />
+        </>
+    )
+}
+
+export default Contact;
