930b5f958dadf02a0861e3b96cdf1ef02e984236
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-20 16:52:25 +0100

    GalleryHeader created, styled and added to Gallery

---
+++ src/components/GalleryHeader/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FormH3, FormSpan } from './styled';
+
+const GalleryHeader = () => {
+    return (
+        <FormH3>Let's <FormSpan>check</FormSpan> out<br />our <FormSpan>work!</FormSpan></FormH3>
+    )
+}
+
+export default GalleryHeader;
---
+++ src/components/GalleryHeader/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FormH3 = styled.h3`
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 200;
+    font-size: 38px;
+    line-height: 1.5;
+    margin: 50px 0;
+    @media (max-width: 768px) {
+        font-size: 20px;
+        margin-top: 30px;
+        margin-bottom: 50px;
+    }
+`;
+export const FormSpan = styled.span`
+    font-weight: bold;
+`;
