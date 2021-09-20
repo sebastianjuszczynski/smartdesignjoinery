8e19df136a76f47eb6b570dda02d391b72b6be80
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-20 12:33:34 +0100

    height of Form Container changed from 600px to 400px

--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
     margin: 10px auto;
     width: 1140px;
     position: relative;
-    height: 600px;
+    height: 400px;
     display: grid;
     grid-template-columns: 1fr 1fr;
     @media (max-width: 768px) {
