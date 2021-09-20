cbc9cace0ae338b9f6302fb2546dabd6221e143a
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-20 16:52:47 +0100

    ScrollToTop added to Gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import GalleryHeader from '../GalleryHeader';
 import Grid from '../Grid';
+import ScrollToTop from '../ScrollToTop';
 
 const Gallery = () => {
     return (
+      <>
+      <GalleryHeader />
       <Grid />
+      <ScrollToTop />
+      </>
     )
 }
 
