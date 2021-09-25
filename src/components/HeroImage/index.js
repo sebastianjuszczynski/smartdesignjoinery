700b73548b0283677dfaf70ad900824fc2b1afd7
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-25 10:11:50 +0100

    two animation created with framer motion

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,6 +1,26 @@
 
 import { Image, Overlay, Text, TextBig, TextSmall, ArrowDown, Arrow, Container } from "./styled";
-import {Link} from "react-scroll";
+import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial: {
+        x: '-100vw'
+    },
+    animate: {
+        x: 0,
+        transition: { type: "spring" }
+    },
+};
+const nextVariants = {
+    initial: {
+        x: '-100vw'
+    },
+    animate: {
+        x: 0,
+        transition: { type: "spring", delay: 0.5 }
+    },
+};
 
 
 
@@ -8,12 +28,22 @@ const HeroImage = () => {
 
     return (
         <Container>
-            
+
             <Image />
             <Overlay />
             <Text>
-                <TextBig>Smart Design Joinery</TextBig>
-                <TextSmall>Vision, Passion, Purpose</TextSmall>
+                <TextBig as={motion.h2}
+                    variants={textVariants}
+                    initial="initial"
+                    animate="animate"
+                >
+                    Smart Design Joinery</TextBig>
+                <TextSmall as={motion.h3}
+                    variants={nextVariants}
+                    initial="initial"
+                    animate="animate"
+                >
+                    Vision, Passion, Purpose</TextSmall>
             </Text>
             <ArrowDown class="arrow bounce">
                 <Arrow><Link to="motto" spy={true} smooth={true}><i class="fas fa-chevron-down"></i></Link></Arrow>
--- src/components/Motto/index.js
+++ src/components/Motto/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Text, Span, ExclamationMark } from './styled'
+import { Text, Span, ExclamationMark } from './styled';
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial: {
+        x: '100vw'
+    },
+    animate: {
+        x: 0,
+        transition: { type: "spring" }
+    },
+};
 
 const Motto = () => {
     return (
-        <Text id="motto">
-            We are not just <Span>building</Span>...<br></br>...we <Span>create </Span><ExclamationMark>!</ExclamationMark>
+        <Text as={motion.h2} id="motto"
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            >
+            We are not just <Span>
+                building</Span>...<br></br>...we <Span>create <ExclamationMark>!</ExclamationMark></Span>
         </Text>
     )
 }
