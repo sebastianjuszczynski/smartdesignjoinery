d0bd07587e0ef33b90a51d2feaba1ffef8fcbfdc
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-25 11:15:47 +0100

    animation on Motto added with useInView and useAnimation

--- src/components/Motto/index.js
+++ src/components/Motto/index.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, Span, ExclamationMark } from './styled';
 import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from "framer-motion";
 
-const textVariants = {
-    initial: {
-        x: '100vw'
-    },
-    animate: {
-        x: 0,
-        transition: { type: "spring" }
-    },
-};
+// const textVariants = {
+//     initial: {
+//         x: -300
+//     },
+//     animate: {
+//         x: 0,
+//         transition: { type: "spring" }
+//     },
+// };
 
 const Motto = () => {
+    const animation = useAnimation();
+    const { ref, inView } = useInView({
+        threshold: 0.3
+    });
+
+
+    useEffect(() => {
+        if (inView) {
+            animation.start('visible');
+        }
+    }, [inView]);
+
     return (
         <Text as={motion.h2} id="motto"
-            variants={textVariants}
-            initial="initial"
-            animate="animate"
-            >
+            ref={ref}
+            animate={animation}
+            initial='hidden'
+            variants={{
+                visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: { type: "spring", stiffness: 300, delay: 0.3, duration: 2 }
+                },
+                hidden: {
+                    x: 250,
+                    opacity: 0
+                }
+            }}
+        >
             We are not just <Span>
                 building</Span>...<br></br>...we <Span>create <ExclamationMark>!</ExclamationMark></Span>
         </Text>
