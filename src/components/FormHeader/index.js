668c43cdac3276ed62c0a8c098bea93926f530e5
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-26 11:25:24 +0100

    animation added to FormHeader

--- src/components/FormHeader/index.js
+++ src/components/FormHeader/index.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router';
 import { FormH3, FormSpan } from './styled';
+import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from "framer-motion";
 
 const FormHeader = (props) => {
+
+    const animation = useAnimation();
+    const { ref, inView } = useInView({
+        threshold: 0.1
+    });
+
+
+    useEffect(() => {
+        if (inView) {
+            animation.start('visible');
+        }
+    }, [inView]);
+
     return (
-        <FormH3 location={props.location.pathname}>Contact <FormSpan>with us</FormSpan> and check<br />what we can do <FormSpan>together!</FormSpan></FormH3>
+        <FormH3 as={motion.h3}
+            ref={ref}
+            variants={{
+                hidden: {
+                    x: -1000
+                },
+                visible: {
+                    x:0,
+                    transition: { type: "spring", stiffness: 150, delay: 0.3, duration: 2 }
+                }
+            }}
+            initial="hidden"
+            animate={animation}
+            location={props.location.pathname}>Contact <FormSpan as={motion.span}
+            ref={ref}
+                variants={{
+                    hidden: {
+                        opacity: 0      
+                      },
+                      visible: {
+                        opacity: 1,      
+                        transition: { 
+                          duration: 3
+                        }
+                      }
+                }} 
+                initial="hidden" 
+                animate={animation}
+            >with us</FormSpan> and check<br />what we can do <FormSpan as={motion.span}
+            ref={ref}
+                variants={{
+                    hidden: {
+                        opacity: 0      
+                      },
+                      visible: {
+                        opacity: 1,      
+                        transition: { 
+                          duration: 3
+                        }
+                      }
+                }} 
+                initial="hidden" 
+                animate={animation}
+            >together!</FormSpan></FormH3>
     )
 }
 
