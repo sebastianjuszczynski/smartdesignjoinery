84a3b6293ed5ae73781fc3a71cb39282d30516ca
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-26 12:59:38 +0100

    Lazy added to Gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react';
 import GalleryHeader from '../GalleryHeader';
-import Grid from '../Grid';
+// import Grid from '../Grid';
 import ScrollToTop from '../ScrollToTop';
 import {Container} from "./styled"
+const Grid = lazy(() => import("../Grid/index"));
 
 const Gallery = () => {
     return (
       <Container>
       <GalleryHeader />
-      <Grid />
+      {/* <Grid /> */}
+      <Suspense fallback={<div>Loading.....</div>}>
+            <Grid />
+          </Suspense>
       <ScrollToTop />
       </Container>
     )
