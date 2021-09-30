d34b7735781856c36d38395e4fc4a239df94b4d7
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-09-30 10:38:56 +0100

    Styles added to section with svg

--- src/components/FormSignup/styled.js
+++ src/components/FormSignup/styled.js
@@ -8,6 +8,9 @@ export const FormInputs = styled.div`
     margin-top: 0.5rem;
     color: #f00e0e; 
     }
+    @media (max-width: 768px) {
+        width: 90%;
+    }
 `;
 export const FormLabel = styled.div`
     display: inline-block;
@@ -56,8 +59,47 @@ export const FormInputButton = styled.button`
     text-transform: uppercase;
     letter-spacing: 2px;
     transition: 0.3s;
+    padding: 10px;
+    @media (max-width: 768px) {
+        width: 90%;
+    }
+   
     &:hover {
         cursor: pointer;
         background: rgb(114, 66, 43);    
+    }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+    margin-bottom: 50px;
+    margin-top: 50px;
+    @media (max-width: 768px) {
+        flex-wrap: wrap;
+        margin-top: 100px;
+        margin-bottom: 25px;
+    }
+`;
+export const ContainerItem = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 5px 30px;
+    @media (max-width: 768px) {
+        margin-left: 0;
+    }
+    
+`;
+
+export const Link = styled.a`
+    text-decoration: none;
+    font-size: 20px;
+    text-decoration: none;
+    color: black;
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
+    &:hover {
+        color: grey;
     }
 `;
