5e5b5f3b9942b98a1a1798e9aa9d146e3b44ded4
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2022-02-03 18:59:32 +0000

    refactored hideDropdown and used it in Navigation component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { NavList, StyledNavLink, StyledNavLink2, SubList, Box } from './styled';
+import { NavList, StyledNavLink, StyledNavLink2, SubList, Box, Dropdown, DropdownContainer } from './styled';
 import { toHome, toWardrobes, toDoors, toStairs, toContact, toPrivacyPolicy } from "../../routes";
 
 const Navigation = ({ open, location }) => {
     const [subnav, setSubnav] = useState(false);
     const showSubnav = () => setSubnav(!subnav);
+
+    const [dropdown, setDropdown] = useState(false);
+    const hideDropdown = () => {
+        setDropdown(false);
+        setSubnav(false)
+    }
 
     const [width, setWidth] = useState(window.innerWidth);
     const breakpoint = 768;
@@ -18,27 +24,31 @@ const Navigation = ({ open, location }) => {
 
     return (
         <NavList open={open}>
-            <StyledNavLink to={toHome()} onClick={() => setSubnav(false)}>Home</StyledNavLink>
+            <StyledNavLink to={toHome()} onClick={hideDropdown}>Home</StyledNavLink>
             {width < breakpoint &&
-            <>
-                <StyledNavLink2 to={toWardrobes()}>Wardrobes</StyledNavLink2>
-                <StyledNavLink2 to={toDoors()}>Doors</StyledNavLink2>
-                <StyledNavLink2 to={toStairs()}>Stairs</StyledNavLink2>
+                <>
+                    <StyledNavLink2 to={toWardrobes()}>Wardrobes</StyledNavLink2>
+                    <StyledNavLink2 to={toDoors()}>Doors</StyledNavLink2>
+                    <StyledNavLink2 to={toStairs()}>Stairs</StyledNavLink2>
                 </>
             }
             {width > breakpoint &&
-            <SubList onClick={showSubnav}>Gallery
-            {subnav &&
-            <Box location={location.pathname}>
-                <StyledNavLink2 to={toWardrobes()}>Wardrobes</StyledNavLink2>
-                <StyledNavLink2 to={toDoors()}>Doors</StyledNavLink2>
-                <StyledNavLink2 to={toStairs()}>Stairs</StyledNavLink2>
-            </Box>
+
+                <SubList onClick={showSubnav}>
+                    <DropdownContainer onClick={() => setDropdown(!dropdown)}>Gallery
+                        <Dropdown subnav={subnav}><i class="fas fa-caret-down"></i></Dropdown>
+                    </DropdownContainer>
+
+                    <Box location={location.pathname} dropdown={dropdown} onClick={hideDropdown}>
+                        <StyledNavLink2 to={toWardrobes()}>Wardrobes</StyledNavLink2>
+                        <StyledNavLink2 to={toDoors()}>Doors</StyledNavLink2>
+                        <StyledNavLink2 to={toStairs()}>Stairs</StyledNavLink2>
+                    </Box>
+
+                </SubList>
             }
-            </SubList>
-            }
-            <StyledNavLink to={toPrivacyPolicy()} onClick={() => setSubnav(false)}>Policy</StyledNavLink>
-            <StyledNavLink to={toContact()} onClick={() => setSubnav(false)}>Contact</StyledNavLink>
+            <StyledNavLink to={toPrivacyPolicy()} onClick={hideDropdown}>Policy</StyledNavLink>
+            <StyledNavLink to={toContact()} onClick={hideDropdown}>Contact</StyledNavLink>
         </NavList>
     )
 }
