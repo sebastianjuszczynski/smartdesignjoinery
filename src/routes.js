f889a51ce2ad2bc6e797f6f40e139ceea32c1912
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2022-02-03 13:30:13 +0000

    Added new routes in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 export const toHome = () => "/home";
-export const toGallery = () => "/gallery";
+// export const toGallery = () => "/gallery";
 export const toContact = () => "/contact";
 export const toPrivacyPolicy = () => "/privacyPolicy";
+
+export const toWardrobes = () => "/wardrobes";
+export const toStairs = () => "/stairs";
+export const toDoors = () => "/doors";
