9a529a5715abb189184ff1d63f991224bb0c8f3a
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2022-02-03 13:33:01 +0000

    Navigation temporarily changed

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,13 @@
 import { NavList, StyledNavLink } from './styled';
-import { toHome, toGallery, toContact, toPrivacyPolicy } from "../../routes";
+import { toHome, toWardrobes, toDoors, toStairs, toContact, toPrivacyPolicy } from "../../routes";
 
 const Navigation = ({ open }) => {
     return (
         <NavList open={open}>
             <StyledNavLink to={toHome()}>Home</StyledNavLink>
-            <StyledNavLink to={toGallery()}>Gallery</StyledNavLink>
+            <StyledNavLink to={toWardrobes()}>Wardrobes</StyledNavLink>
+            <StyledNavLink to={toDoors()}>Doors</StyledNavLink>
+            <StyledNavLink to={toStairs()}>Stairs</StyledNavLink>
             <StyledNavLink to={toPrivacyPolicy()}>Policy</StyledNavLink>
             <StyledNavLink to={toContact()}>Contact</StyledNavLink>
         </NavList>
